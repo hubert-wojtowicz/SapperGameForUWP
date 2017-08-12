82b26d01255c1267bddb57a3dbc9b633ff992096
Hubert Wójtowicz <hubert.karol.wojtowicz@gmail.com>
2017-08-12 15:49:30 +0200

    There is panel generated on canvas.

--- SapperGameView/SapperPanel.cs
+++ SapperGameView/SapperPanel.cs
@@ -17,7 +17,7 @@ namespace SapperGameView
 
         // Using a DependencyProperty as the backing store for HorizontalTilesNumber.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty HorizontalTilesNumberProperty =
-            DependencyProperty.Register("HorizontalTilesNumber", typeof(int), typeof(SapperPanelView), new PropertyMetadata(200));
+            DependencyProperty.Register("HorizontalTilesNumber", typeof(int), typeof(SapperPanelView), new PropertyMetadata(8));
 
 
 
@@ -29,7 +29,7 @@ namespace SapperGameView
 
         // Using a DependencyProperty as the backing store for VerticalTilesNumber.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty VerticalTilesNumberProperty =
-            DependencyProperty.Register("VerticalTilesNumber", typeof(int), typeof(SapperPanelView), new PropertyMetadata(200));
+            DependencyProperty.Register("VerticalTilesNumber", typeof(int), typeof(SapperPanelView), new PropertyMetadata(8));
 
 
         public int SquareTileSize
@@ -40,7 +40,7 @@ namespace SapperGameView
 
         // Using a DependencyProperty as the backing store for SquareTileSize.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty SquareTileSizeProperty =
-            DependencyProperty.Register("SquareTileSize", typeof(int), typeof(SapperPanelView), new PropertyMetadata(5));
+            DependencyProperty.Register("SquareTileSize", typeof(int), typeof(SapperPanelView), new PropertyMetadata(30));
 
 
 
@@ -52,13 +52,17 @@ namespace SapperGameView
 
         // Using a DependencyProperty as the backing store for BombDensityPercent.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty BombDensityPercentProperty =
-            DependencyProperty.Register("BombDensityPercent", typeof(int), typeof(SapperPanelView), new PropertyMetadata(0));
+            DependencyProperty.Register("BombDensityPercent", typeof(int), typeof(SapperPanelView), new PropertyMetadata(20));
 
         #endregion
 
         Button startGameBtm;
+        TextBlock ex = new TextBlock();
+        Coordinate startGUI = null;
 
         ISapperGamePanelOperations panelLogic;
+
+        Button[,] panelTile;
 
         public SapperPanelView()
         {
@@ -71,8 +75,44 @@ namespace SapperGameView
             startGameBtm.Content = "Start";
             startGameBtm.Click += RecreateGamePanel_EventHandler;
 
+            startGUI = new Coordinate(10, 70);
 
-            Children.Add(startGameBtm);
+            this.Loaded += SapperPanelView_Loaded;
+        }
+
+        private void SapperPanelView_Loaded(object sender, RoutedEventArgs e)
+        {
+            GenerateNewPanel();
+        }
+
+        private void GenerateNewPanel()
+        {
+            panelTile = new Button[HorizontalTilesNumber, VerticalTilesNumber];
+
+            for (int i = 0; i < VerticalTilesNumber; i++)
+            {
+                for (int j = 0; j < HorizontalTilesNumber; j++)
+                {
+                    panelTile[i, j] = new Button();
+                    panelTile[i, j].Height = SquareTileSize;
+                    panelTile[i, j].Width = SquareTileSize;
+
+                    panelTile[i, j].SetValue(Canvas.TopProperty, startGUI.vertical + i * (SquareTileSize + 5));
+                    panelTile[i, j].SetValue(Canvas.LeftProperty, startGUI.horizontal + j * (SquareTileSize + 5));
+
+                    panelTile[i, j].Content = "?";
+                    panelTile[i, j].FontSize -= 5;
+
+                    panelTile[i, j].Click += SapperPanelView_Click;
+
+                    Children.Add(panelTile[i, j]);
+                }
+            }
+        }
+
+        private void SapperPanelView_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         private void RecreateGamePanel_EventHandler(object sender, RoutedEventArgs e)
@@ -87,7 +127,7 @@ namespace SapperGameView
             }
             catch (Exception ex)
             {
-                panelLogic = new SapperGamePanelModel(10,10);
+                panelLogic = new SapperGamePanelModel(10, 10);
             }
 
             ((Button)sender).Content = $"h={panelLogic.GetHorizontalSize}; v={panelLogic.GetVerticalSize}; p={panelLogic.GetBombDensityPercent}";
