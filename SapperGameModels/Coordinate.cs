8d8cd2bf57a5997ccff39b2b1d4f884e0daac169
Hubert Wójtowicz <hubert.karol.wojtowicz@gmail.com>
2017-08-12 09:52:10 +0200

    Class responsible for position on game panel added.

---
+++ SapperGameModels/Coordinate.cs
@@ -0,0 +1,23 @@
+﻿namespace Saper.Model
+{
+    public class Coordinate
+    {
+        public int horizontal;
+        public int vertical;
+
+        public Coordinate()
+        { }
+
+        public Coordinate(int hor, int ver)
+        {
+            horizontal = hor;
+            vertical = ver;
+        }
+
+        public Coordinate(Coordinate currentPosition, int horizontalShift, int verticalShift)
+        {
+            horizontal = currentPosition.horizontal + horizontalShift;
+            vertical = currentPosition.vertical + verticalShift;
+        }
+    }
+}
