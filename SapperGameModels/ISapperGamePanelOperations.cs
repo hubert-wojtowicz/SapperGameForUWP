e38c22e66b446d3a9d3081e14725af11a918d019
Hubert Wójtowicz <hubert.karol.wojtowicz@gmail.com>
2017-08-12 11:23:32 +0200

    Game model created.

---
+++ SapperGameModels/ISapperGamePanelOperations.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Saper.Model
+{
+    public interface ISapperGamePanelOperations
+    {
+        ushort GetNumberOfAdjacentBombsIn(Coordinate field);
+
+        void UncoverZerosAndAdjacentIn(Coordinate currentField, ref List<Coordinate> listOfUncovered);
+
+        void UncoverOneIn(Coordinate field);
+
+        bool LeftFieldsUncoveredWithoutBomb();
+
+        bool IsFieldUncovered(Coordinate field);
+
+        bool IsFieldInsidePanel(Coordinate field);
+
+        bool IsBombInside(Coordinate field);
+    };
+}
