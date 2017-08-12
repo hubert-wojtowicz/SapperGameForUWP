f3a468596821b7d51d1bd3b2c629a0d4a7f97cbe
Hubert Wójtowicz <hubert.karol.wojtowicz@gmail.com>
2017-08-12 22:24:10 +0200

    Tiles on panel uncovering works.

--- SapperGameView/SapperPanel.cs
+++ SapperGameView/SapperPanel.cs
@@ -1,14 +1,16 @@
 ﻿using Saper.Model;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 
 namespace SapperGameView
 {
     public class SapperPanelView : Canvas
     {
-
         #region dependency properties
         public int HorizontalTilesNumber
         {
@@ -55,9 +57,21 @@ namespace SapperGameView
         public static readonly DependencyProperty BombDensityPercentProperty =
             DependencyProperty.Register("BombDensityPercent", typeof(int), typeof(SapperPanelView), new PropertyMetadata(20));
 
+
+
+        public int TilesMargin
+        {
+            get { return (int)GetValue(TilesMarginProperty); }
+            set { SetValue(TilesMarginProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for TilesMargin.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TilesMarginProperty =
+            DependencyProperty.Register("TilesMargin", typeof(int), typeof(SapperPanelView), new PropertyMetadata(3));
+
+
         #endregion
 
-        Button startGameBtm;
         TextBlock ex = new TextBlock();
         Coordinate startGUI = null;
 
@@ -68,25 +82,14 @@ namespace SapperGameView
 
         public SapperPanelView()
         {
-            startGameBtm = new Button();
-            startGameBtm.SetValue(Canvas.TopProperty, 10);
-            startGameBtm.SetValue(Canvas.LeftProperty, 10);
-
-            startGameBtm.Height = 50;
-            startGameBtm.Width = 150;
-            startGameBtm.Content = "Start";
-            startGameBtm.Click += RecreateGamePanel_EventHandler;
-
-            Children.Add(startGameBtm);
-
-            startGUI = new Coordinate(10, 70);
+            startGUI = new Coordinate(TilesMargin, TilesMargin);
 
             this.Loaded += SapperPanelView_Loaded;
         }
 
         private void SapperPanelView_Loaded(object sender, RoutedEventArgs e)
         {
-            GenerateNewPanel();
+            RecreateGamePanel_EventHandler(this, new RoutedEventArgs());
         }
 
         private int Trans2DTo1D(int horzontalCoordinate, int verticalCoordinate)
@@ -112,8 +115,8 @@ namespace SapperGameView
                     panelTile[currentPos].Height = SquareTileSize;
                     panelTile[currentPos].Width = SquareTileSize;
 
-                    panelTile[currentPos].SetValue(Canvas.TopProperty, startGUI.vertical + j * (SquareTileSize + 5));
-                    panelTile[currentPos].SetValue(Canvas.LeftProperty, startGUI.horizontal + i * (SquareTileSize + 5));
+                    panelTile[currentPos].SetValue(Canvas.TopProperty, startGUI.vertical + j * (SquareTileSize + TilesMargin));
+                    panelTile[currentPos].SetValue(Canvas.LeftProperty, startGUI.horizontal + i * (SquareTileSize + TilesMargin));
 
                     panelTile[currentPos].Content = "?";
                     panelTile[currentPos].FontSize -= 5;
@@ -127,8 +130,9 @@ namespace SapperGameView
 
         private void destroyCurrentPanel()
         {
-            Children.Clear();
-            panelTile.Clear();
+            // czyszczenie dzieci czyści też przycisk na górze
+            Children?.Clear();
+            panelTile?.Clear();
         }
 
         private void SapperPanelView_Click(object sender, RoutedEventArgs e)
@@ -137,10 +141,43 @@ namespace SapperGameView
 
             int pos = panelTile.FindIndex(x => x == currentBtn);
 
-            Coordinate cord = Trans1DTo2D(pos);
+            Coordinate coordOfClicked = Trans1DTo2D(pos);
 
-            startGameBtm.Content = $"({cord.horizontal},{cord.vertical})";
 
+            if (!panelLogic.IsFieldUncovered(coordOfClicked))
+            {
+                if (!panelLogic.IsBombInside(coordOfClicked))
+                {
+                    int numOfAdjBombs = panelLogic.GetNumberOfAdjacentBombsIn(coordOfClicked);
+
+                    if (numOfAdjBombs > 0)
+                    {
+                        panelLogic.UncoverOneIn(coordOfClicked);
+                        panelTile[Trans2DTo1D(coordOfClicked.horizontal, coordOfClicked.vertical)].Content = numOfAdjBombs.ToString();
+                        panelTile[Trans2DTo1D(coordOfClicked.horizontal, coordOfClicked.vertical)].Background = this.Background;
+                    }
+                    else
+                    {
+                        List<Coordinate> lista = new List<Coordinate>();
+                        panelLogic.UncoverZerosAndAdjacentIn(coordOfClicked, ref lista);
+
+
+                        foreach (var item in lista)
+                        {
+                            panelTile[Trans2DTo1D(item.horizontal, item.vertical)].Content =
+                                panelLogic.GetNumberOfAdjacentBombsIn(item).ToString();
+
+                            panelTile[Trans2DTo1D(item.horizontal, item.vertical)].Background = this.Background;
+                        }
+                    }
+                }
+                else
+                {
+                    panelTile[Trans2DTo1D(coordOfClicked.horizontal, coordOfClicked.vertical)].Content = "*";
+                    panelTile[Trans2DTo1D(coordOfClicked.horizontal, coordOfClicked.vertical)].Background = new SolidColorBrush(Colors.Red);
+                    Debug.WriteLine("Game over");
+                }
+            }
         }
 
         private void RecreateGamePanel_EventHandler(object sender, RoutedEventArgs e)
@@ -157,8 +194,11 @@ namespace SapperGameView
             {
                 panelLogic = new SapperGamePanelModel(10, 10);
             }
-
-            ((Button)sender).Content = $"h={panelLogic.GetHorizontalSize}; v={panelLogic.GetVerticalSize}; p={panelLogic.GetBombDensityPercent}";
+            finally
+            {
+                destroyCurrentPanel();
+                GenerateNewPanel();
+            }
         }
     }
 }
